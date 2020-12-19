905fe66c528eb6d7b40926e65d85d851857dbda0
Brian Chen <bc844@drexel.edu>
2020-12-19 13:25:40 -0500

    Figure out how to use Reactor

--- src/main/kotlin/io/github/bchen290/drexelcoursebot/CourseCog.kt
+++ src/main/kotlin/io/github/bchen290/drexelcoursebot/CourseCog.kt
@@ -6,9 +6,19 @@ import reactor.core.publisher.Mono
 class CourseCog(commands: MutableMap<String, Command>) {
     init {
         commands["ping"] = Command { event ->
-            Mono.justOrEmpty(event.message.content)
-                .map { content -> content.split(" ") }
-                .doOnNext { command -> event.message.channel.flatMap { channel -> channel.createMessage(command.joinToString { it }) }.subscribe() }
+            Mono.justOrEmpty(event.message)
+                .map { Pair(it.content, it.channel) }
+                .doOnNext { it.second.flatMap { channel -> channel.createMessage("Pong") }.subscribe() }
+                .then()
+        }
+
+        commands["course"] = Command { event ->
+            Mono.justOrEmpty(event.message)
+                .map { Pair(it.content.split(" "), it.channel) }
+                .doOnNext { pair ->
+                    val (content, channel) = pair
+                    channel.flatMap { it.createMessage(content.joinToString { it }) }.subscribe()
+                }
                 .then()
         }
     }
